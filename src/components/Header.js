2cd5b232bb6242c5482eb65bea058880a156c27b
Elijah <andersene909@gmail.com>
2019-07-01 15:09:16 -0700

    Simple sidebar added;

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,25 @@ import React, { Component } from 'react';
 import './Header.css';
 
 export default class Header extends Component {
+  state = {
+    clicked: false
+  }
+  openSidebar() {
+    this.setState(prevState => ({
+      clicked: !prevState.clicked
+    }));
+  }
   render() {
     return (
-      <div className="header-container">
+      <React.Fragment>
+        <div className="header-container">
+          <h2 onClick={() => this.openSidebar()}>Open Sidebar</h2>
+        </div>
+        <div className={`side-container ${this.state.clicked ? 'active' : 'hidden'}`}>
 
-      </div>
+        </div>
+      </React.Fragment>
+
     )
   }
 }
