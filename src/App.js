9de454986c5b068568e9c4156e1ea06346438862
Elijah <andersene909@gmail.com>
2019-07-05 11:49:33 -0700

    Actions; Reducers; Store added for initial setup; Will need to come back to these once the needed state is more clearly defined;

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import { Route, BrowserRouter as Router } from 'react-router-dom';
-import './App.css';
+import rootReducer from './_reducers';
 
 import PageLayout from './components/Layout/PageLayout';
 
-import Login from './routes/Login';
 import Home from './routes/Home';
 
-// Redux Store and BrowserRouter need to be implemented here
+const store = createStore(rootReducer);
 
 // Logged in state and validation at the highest level so the user would be kicked back to login from here.
 // Additionally, pass the logged in state down to PageLayout and hide the navigation options if not logged in.
 
 function App() {
   return (
-    <PageLayout>
-      <Router>
-        <Route exact path="/" component={Home} />
-      </Router>
-    </PageLayout>
+    <Provider store={store}>
+      <PageLayout>
+        <Router>
+          <Route exact path="/" component={Home} />
+        </Router>
+      </PageLayout>
+    </Provider>
   );
 }
 
---
+++ src/_actions/index.js
@@ -0,0 +1,4 @@
+// Placeholder file until the needed state is more detailed
+export const placeholder = {
+  TEST: 'TEST'
+}
