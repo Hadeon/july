9786897071c95a34963d255bfa10a635d5c54c8a
Elijah <andersene909@gmail.com>
2019-07-05 13:06:13 -0700

    react-google-login added to dependencies and .env added to gitignore;

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import GoogleLogin from 'react-google-login';
 
 // Check if the user exists in the DB, if not:
 // Add the user to DB in order to track order history and whatnot
+
+const responseGoogle = (res) => {
+  console.log(res);
+}
 
 const Login = () => {
   return (
     <div>
       <p>Login</p>
+      <GoogleLogin 
+        clientId=""
+        buttonText="Login" 
+        onSuccess={responseGoogle} 
+        onFailure={responseGoogle}
+        cookiePolicy={'single-host-origin'}
+      />
     </div>
   )
 }
