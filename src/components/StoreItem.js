a2524d896302f9cbd16df99c56d1236a4de7f9ea
Hadeon <andersene909@gmail.com>
2019-07-08 15:16:28 -0700

    Experimenting with react-intersection-observer to display only StoreItems in view;

--- src/components/StoreItem.js
+++ src/components/StoreItem.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
+import { useInView } from 'react-intersection-observer';
 import './StoreItem.css';
 
 const StoreItem = () => {
+  const [ref, inView] = useInView({
+    threshold: 0
+  })
+
   return (
-    <div className="store-item-container">
-      <h3>This is an item</h3>
-      <p>Some details. It's a thing. It's great. Now buy it please.</p>
-      <h4>$10</h4>
+    <div className="store-item-container" ref={ref}>
+      { inView ? (
+        <React.Fragment>
+          <h3>This is an item</h3>
+          <p>Some details. It's a thing. It's great. Now buy it please. {`${inView}`}</p>
+          <h4>$10</h4>
+        </React.Fragment>
+      ) : <React.Fragment/> }
+
     </div>
   )
 }
