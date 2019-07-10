3923bc7519cd25d1de90c66c10d42c6cf46c072c
Hadeon <andersene909@gmail.com>
2019-07-10 15:08:11 -0700

    Slightly tweaked MainContainer.css so that the sidebar DOESNT push the MainContainer to the right if under 700px; StoreItem now uses useInView to lazy load contents; Testing Suspense and React.lazy in Home.js;

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -4,9 +4,33 @@ import './Home.css';
 const StoreItem = React.lazy(() => import('../components/StoreItem'));
 
 export default class Home extends Component {
+  state = {
+    count: 5
+  }
   render() {
     return (
       <div style={{ display: 'flex', flexDirection: 'row', flexFlow: 'row wrap' }}>
+        <Suspense fallback={<div>...Loading</div>}>
+          <StoreItem/>
+        </Suspense>
+        <Suspense fallback={<div>...Loading</div>}>
+          <StoreItem/>
+        </Suspense>
+        <Suspense fallback={<div>...Loading</div>}>
+          <StoreItem/>
+        </Suspense>
+        <Suspense fallback={<div>...Loading</div>}>
+          <StoreItem/>
+        </Suspense>
+        <Suspense fallback={<div>...Loading</div>}>
+          <StoreItem/>
+        </Suspense>
+        <Suspense fallback={<div>...Loading</div>}>
+          <StoreItem/>
+        </Suspense>
+        <Suspense fallback={<div>...Loading</div>}>
+          <StoreItem/>
+        </Suspense>
         <Suspense fallback={<div>...Loading</div>}>
           <StoreItem/>
         </Suspense>
