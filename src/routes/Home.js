79b71be66204f4d300eeb9a92f7a3d133170b255
Hadeon <andersene909@gmail.com>
2019-07-10 15:50:35 -0700

    Testing to make sure Google Login works before moving on to setting the logged in state and rendering based off this;

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -5,53 +5,16 @@ const StoreItem = React.lazy(() => import('../components/StoreItem'));
 
 export default class Home extends Component {
   state = {
-    count: 5
+    count: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
   }
   render() {
     return (
       <div style={{ display: 'flex', flexDirection: 'row', flexFlow: 'row wrap' }}>
-        <Suspense fallback={<div>...Loading</div>}>
-          <StoreItem/>
-        </Suspense>
-        <Suspense fallback={<div>...Loading</div>}>
-          <StoreItem/>
-        </Suspense>
-        <Suspense fallback={<div>...Loading</div>}>
-          <StoreItem/>
-        </Suspense>
-        <Suspense fallback={<div>...Loading</div>}>
-          <StoreItem/>
-        </Suspense>
-        <Suspense fallback={<div>...Loading</div>}>
-          <StoreItem/>
-        </Suspense>
-        <Suspense fallback={<div>...Loading</div>}>
-          <StoreItem/>
-        </Suspense>
-        <Suspense fallback={<div>...Loading</div>}>
-          <StoreItem/>
-        </Suspense>
-        <Suspense fallback={<div>...Loading</div>}>
-          <StoreItem/>
-        </Suspense>
-        <Suspense fallback={<div>...Loading</div>}>
-          <StoreItem/>
-        </Suspense>
-        <Suspense fallback={<div>...Loading</div>}>
-          <StoreItem/>
-        </Suspense>
-        <Suspense fallback={<div>...Loading</div>}>
-          <StoreItem/>
-        </Suspense>
-        <Suspense fallback={<div>...Loading</div>}>
-          <StoreItem/>
-        </Suspense>
-        <Suspense fallback={<div>...Loading</div>}>
-          <StoreItem/>
-        </Suspense>
-        <Suspense fallback={<div>...Loading</div>}>
-          <StoreItem/>
-        </Suspense>
+        {this.state.count.map((item, index) => (
+          <Suspense fallback={<div>...Loading</div>} key={index}>
+            <StoreItem/>
+          </Suspense>
+        ))}
       </div>
     )
   }
