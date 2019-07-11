2c54b6f9d89ca81cbde8e1f5053bab97d7cc6b8a
Hadeon <andersene909@gmail.com>
2019-07-10 22:29:48 -0700

    Rendering the StoreItems within Home component with ...Array mapping rather than having to manually build an array;

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -5,12 +5,12 @@ const StoreItem = React.lazy(() => import('../components/StoreItem'));
 
 export default class Home extends Component {
   state = {
-    count: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+    count: 10
   }
   render() {
     return (
       <div style={{ display: 'flex', flexDirection: 'row', flexFlow: 'row wrap' }}>
-        {this.state.count.map((item, index) => (
+        {[...Array(this.state.count)].map((count, index) => (
           <Suspense fallback={<div>...Loading</div>} key={index}>
             <StoreItem/>
           </Suspense>
